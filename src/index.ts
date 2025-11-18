c8bed22204173fb7f0795a6776e759e342929375
Erackson souza <souza.erackson@gmail.com>
2025-11-18 20:03:59 -0300

    feat: initial estruture

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-function calculateAgeOfUser ( user) {
+interface User {
+    birthYear: number
+}
+
+function calculateAgeOfUser ( user: User) {
     return new Date().getFullYear() - user.birthYear;
 } 
 
-calculateAgeOfUser('Erackson')
-calculateAgeOfUser()
+calculateAgeOfUser({
+    birthYear
+})
 
 //Isso eh pq o javascript eh fracamente tipado, ou seja, ele nao obriga a gente a passar o tipo certo de dado para a funcao.
 //Runtime type checking: ou seja, a verificacao de tipos acontece em tempo de execucao, e nao em tempo de compilacao.
