fab3d6be1b00f133f9c0900f7105fe040f3edcfe
Erackson souza <souza.erackson@gmail.com>
2025-11-19 19:08:33 -0300

    feat: add routing test

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
 import fastify from "fastify";
 import { database } from "./database.js";
+import crypto from "node:crypto";
+import { title } from "node:process";
 
 const app = fastify();
 
 app.get("/hello", async () => {
-  const tables = await database("sqlite_schema").select("*");
+  /* 
+  const transaction = await database("transactions")
+    .insert({
+      id: crypto.randomUUID(),
+      title: "transaçao de teste",
+      amount: 1000,
+    })
+    .returning("*");
+*/
+  const transaction = await database("transactions")
+    .where("amount", 500)
+    .select("*");
 
-  return tables;
+  return transaction;
 });
 
 app
