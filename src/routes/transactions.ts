a7db36f1df2d1d39d6e3df038b4f9e0ddf694c9b
Erackson souza <souza.erackson@gmail.com>
2025-11-20 11:21:14 -0300

    feat: implement transaction creation route with validation and response handling

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,14 +1,30 @@
 import type { FastifyInstance } from "fastify";
+import { z } from "zod";
 import { database } from "../database.js";
-
-
+import { title } from "process";
+import { randomUUID } from "crypto";
 
 export async function transactionRoutes(app: FastifyInstance) {
-  app.get("/hello", async () => {
-    const transaction = await database("transactions")
-      .where("amount", 500)
-      .select("*");
+  app.post("/", async (request, reply) => {
+    //{ title, amount, type: credit or debit}
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
+    });
 
-    return transaction;
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body
+    );
+
+    await database("transactions").insert({
+      id: randomUUID(),
+      title,
+      amount: type === "credit" ? amount : amount * -1,
+    });
+
+    //http code
+
+    return reply.status(201).send('deu bom, meu parceiro');
   });
 }
