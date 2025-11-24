6585895d65cd82311c8318f78b6a7f0cf096c489
Erackson souza <souza.erackson@gmail.com>
2025-11-24 16:11:21 -0300

    refactor: clean up transaction tests and improve code formatting

--- src/test/transactions.spec.ts
+++ src/test/transactions.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test, beforeAll, afterAll, beforeEach } from "vitest";
-import {  execSync } from 'node:child_process'
+import { execSync } from "node:child_process";
 import request from "supertest";
 import { app } from "../app.js";
 import { describe } from "node:test";
@@ -13,14 +13,13 @@ describe("Transações API", () => {
     await app.close();
   });
 
-  //isso significa que a cada teste vai rodar uma migracao nova no banco de dados para garantir que o banco de dados esteja sempre limpo antes de cada teste
-  beforeEach(( ) => {{
-    execSync('npm run knex migrate:rollback --all')
-    execSync('npm run knex migrate:latest')
-  }})
+  beforeEach(() => {
+    {
+      execSync("npm run knex migrate:rollback --all");
+      execSync("npm run knex migrate:latest");
+    }
+  });
 
-  //eh possivel tambem usar o it ao inves do test, mas eh opcional
-  //exemplo: it("O usuario consegue consegue criar uma nova transaçao", async () => {
   test("O usuario consegue consegue criar uma nova transaçao", async () => {
     await request(app.server)
       .post("/transactions")
@@ -32,12 +31,7 @@ describe("Transações API", () => {
       .expect(201);
   });
 
-  //test.skip pula o teste
-  //test.todo marca o teste como pendente
-  //test.only roda somente esse teste
   test("O usuario consegue listar todas as transaçoes", async () => {
-    //Todo teste deve obrigatoriamente se excluir de qualquer contexto, ou seja, nao pode ter um teste dependendo de outro teste
-
     const createTransationResponse = await request(app.server)
       .post("/transactions")
       .send({
@@ -53,9 +47,7 @@ describe("Transações API", () => {
       .set("Cookie", cookies)
       .expect(200);
 
-      //estou esperando que a resposta tenha um array de transaçoes dentro do body da resposta transactions
     expect(listTransactionsResponse.body.transactions).toEqual([
-      //aqui no caso eu vou esperar que tenha essa transaçao dentro do array de transaçoes criadas
       expect.objectContaining({
         title: "New transaction",
         amount: 5000,
